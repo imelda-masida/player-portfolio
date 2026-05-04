fbb63398cc537cad65b113255e6337c85959b889
stone <imeldamasidakakudji@gmail.com>
2026-05-05 00:38:39 +0200

    creation des fichiers et dossier puis implementation index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class = "player-container">
+        <img src="assets/player.jpeg" alt="Player" id="player-image">
+        <div class="bulle-dialogue">"YO! ar'you ready to see my stats"</div>
+
+    </div>
+</body>
+</html>
